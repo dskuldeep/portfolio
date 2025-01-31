759ef0e9a5d5810d99cd6ef04f2c9bd80cd60f2e
Kuldeep Paul <kuldeeppaul@Kuldeeps-MacBook-Air.local>
2025-02-01 00:14:05 +0530

    posthog fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import type React from "react"
-import posthog from 'posthog-js'
 
-posthog.init('phc_An1y3jbrNgWRUmsedVIkVhddPBp84FHWJFTsrsy4f1y',
-    {
-        api_host: 'https://us.i.posthog.com',
-        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-    }
-)
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
