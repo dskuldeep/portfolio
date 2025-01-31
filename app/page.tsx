e7a4d2fb4991f523e32dd57d5afddc20d59c38ac
Kuldeep Paul <kuldeeppaul@Kuldeeps-MacBook-Air.local>
2025-02-01 00:24:41 +0530

    no posthog

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,7 @@ import Skills from "./components/Skills"
 import Education from "./components/Education"
 import Achievements from "./components/Achievements"
 import Footer from "./components/Footer"
-import posthog from 'posthog-js'
 
-posthog.init('phc_An1y3jbrNgWRUmsedVIkVhddPBp84FHWJFTsrsy4f1y',
-    {
-        api_host: 'https://us.i.posthog.com',
-        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-    }
-)
 export default function Home() {
   return (
     <div className="min-h-screen">
