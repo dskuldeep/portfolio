05291ad75407716df1ef662390b308c731dad861
Kuldeep Paul <kuldeeppaul@Kuldeeps-MacBook-Air.local>
2025-02-01 00:39:12 +0530

    posthog err

--- app/providers.js
+++ app/providers.js
@@ -6,9 +6,10 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined') {
   posthog.init("phc_An1y3jbrNgWRUmsedVIkVhddPBp84FHWJFTsrsy4f1y", {
     api_host: "https://us.i.posthog.com",
-    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+    person_profiles: 'identified-only', // or 'always' to create profiles for anonymous users as well
   })
 }
 export function CSPostHogProvider({ children }) {
     return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
+
